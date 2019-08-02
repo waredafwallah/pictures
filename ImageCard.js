0da3705f054cef75664adccb3ca2abb979bcfc71
waredafwallah <52910997+waredafwallah@users.noreply.github.com>
2019-08-02 10:28:53 +0200

    Add files via upload

---
+++ ImageCard.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+
+class ImageCard extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { span: 0}
+
+        this.imageRef = React.createRef()
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight
+
+        const spans = Math.ceil(height / 10)
+
+        this.setState({spans})
+    }
+
+
+    render() {
+        const {description, urls} = this.props.image
+
+        return (
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
+                <img alt={description}
+                     src={urls.regular}
+                     ref={this.imageRef}/>
+            </div>
+        )
+    }
+}
+
+export default ImageCard
